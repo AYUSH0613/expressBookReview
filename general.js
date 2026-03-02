1f6aa0e84357ff5cdfe2341fc73257cae11e904a
Ayush Gupta <119103996+AYUSH0613@users.noreply.github.com>
2026-03-02 18:39:02 +0530

    Create general.js

---
+++ general.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+// Get all books
+async function getAllBooks() {
+  const response = await axios.get('http://localhost:5000/');
+  return response.data;
+}
+
+// Get book by ISBN
+async function getBookByISBN(isbn) {
+  const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+  return response.data;
+}
+
+// Get books by author
+async function getBooksByAuthor(author) {
+  const response = await axios.get(`http://localhost:5000/author/${author}`);
+  return response.data;
+}
+
+// Get books by title
+async function getBooksByTitle(title) {
+  const response = await axios.get(`http://localhost:5000/title/${title}`);
+  return response.data;
+}
+
+module.exports = {
+  getAllBooks,
+  getBookByISBN,
+  getBooksByAuthor,
+  getBooksByTitle
+};
